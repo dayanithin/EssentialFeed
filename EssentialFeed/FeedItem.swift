059cad92eda8128a3eab7a334e5743734bf3e4dc
Dayanithi Natarajan <dayanithi.n@telliant.net>
2022-12-31 20:34:00 +0530

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Dayanithi on 31/12/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: String
+}
