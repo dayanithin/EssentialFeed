9b1447d94bb4e9111f603cda52f6ab2037843377
Dayanithi Natarajan <dayanithi.n@telliant.net>
2023-02-20 19:38:54 +0530

    Load command deletes more than seven days old cache upon retrieval

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -114,6 +114,16 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         store.completeRetrival(with: feed.local, timestamp: sevenDaysOldTimestamp)
         XCTAssertEqual(store.recievedMessages, [.retreive, .deleteCacheFeed])
     }
+    
+    func test_load_deleteCacheOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(days: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        sut.load { _ in }
+        store.completeRetrival(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
+        XCTAssertEqual(store.recievedMessages, [.retreive, .deleteCacheFeed])
+    }
 
     private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: LocalFeedLoader.LoadResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
