05f0b9fa6f941ef099fbe33082fef4fb2b51e51c
Dayanithi Natarajan <dayanithi.n@telliant.net>
2023-03-07 23:59:03 +0530

    Extract duplicate delete test code into a reusable helper method

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -180,14 +180,9 @@ class CodableFeedStoreTests: XCTestCase {
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache deletion")
-
-        sut.deleteCachedFeed { deletionError in
-            XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-
+        
+        let deletionError = deleteCache(from: sut)
+        XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed.")
         expect(sut, toRetrieve: .empty)
     }
     
@@ -195,13 +190,8 @@ class CodableFeedStoreTests: XCTestCase {
             let sut = makeSUT()
             insert((uniqueImageFeed().local, Date()), to: sut)
 
-            let exp = expectation(description: "Wait for cache deletion")
-            sut.deleteCachedFeed { deletionError in
-                XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
-                exp.fulfill()
-            }
-            wait(for: [exp], timeout: 1.0)
-
+            let deletionError = deleteCache(from: sut)
+            XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed.")
             expect(sut, toRetrieve: .empty)
         }
     
@@ -246,6 +236,17 @@ class CodableFeedStoreTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1.0)
     }
+    
+    private func deleteCache(from sut: CodableFeedStore) -> Error? {
+            let exp = expectation(description: "Wait for cache deletion")
+            var deletionError: Error?
+            sut.deleteCachedFeed { receivedDeletionError in
+                deletionError = receivedDeletionError
+                exp.fulfill()
+            }
+            wait(for: [exp], timeout: 1.0)
+            return deletionError
+        }
     
     private func testSpecificStoreURL() -> URL {
         FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
