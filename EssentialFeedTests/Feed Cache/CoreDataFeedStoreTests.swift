7d774a6b0225c7d9885f20b2ea1df9575eb14edf
Dayanithi Natarajan <dayanithi.n@telliant.net>
2023-03-27 11:28:08 +0530

    Add empty CoreData feed store specs

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,60 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dayanithi on 27/03/23.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        <#code#>
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        <#code#>
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        <#code#>
+    }
+    
+    
+}
